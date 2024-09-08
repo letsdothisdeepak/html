45bd004f7e4de03ccf3f68a40f5d141339ee0eec
Deepak <workwithmedeepak@gmail.com>
2024-09-08 09:10:59 +0530

    Math and Numbers Basics

---
+++ letsdothisdeepak/JS-Numbers_Math/index.js
@@ -0,0 +1,26 @@
+// let MyNumber= new Number(100.0304)
+
+// console.log(MyNumber);
+// let a= console.log(MyNumber.toString().length);
+// let b= console.log(MyNumber.toFixed(5));
+// let d= console.log(MyNumber.toPrecision(6));
+// let c= console.log(MyNumber.valueOf());
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(d));
+// console.log(typeof(c));
+
+// let NewNumber= new Number(2345)
+
+// console.log(NewNumber);
+// console.log(Math.PI);
+// console.log(Math.ceil(8.34));
+// console.log(Math.floor(8.34));
+
+
+/* Useful method to develop a game like ludo */
+
+let min=1;
+let max=6;
+
+console.log(Math.ceil(Math.random()*(max-min+1)));
---
+++ letsdothisdeepak/JS-String/index.js
@@ -0,0 +1,26 @@
+// let MyName= "Deepak"
+
+// console.log(MyName.indexOf("k"));
+
+// let MyNewName = new String ("Deepak")
+// console.log(MyName[3]);
+// console.log(MyName.toUpperCase());
+// console.log(MyName.toLowerCase());
+// console.log(MyName.length);
+// console.log(MyName.charAt(23));
+// console.log(MyName.slice(2,-3));
+// console.log(MyName.substr(4));
+// console.log(MyName.concat(Rahul,MyName));
+
+
+// text3= console.log(text1.concat(" ",text2));
+
+// console.log(text1.trim());
+// console.log(text1.split("#"));
+// console.log(text1.padEnd(10,"*"));
+// console.log(text1.replace("Deepak","Kumar"));
+
+// console.log(`My Name is ${text1} and my title is ${text2}`);
+// console.log(text1.replace("e","a"));
+console.log(text1.repeat(3));
+console.log(text1.blink());
