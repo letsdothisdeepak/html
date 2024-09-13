00f49f45597e2f186e8983e98f5a3405c9cd705b
Deepak <workwithmedeepak@gmail.com>
2024-09-13 12:02:13 +0530

    Card-Project Using HTML and CSS

--- letsdothisdeepak/JS-Date/index.js
+++ letsdothisdeepak/JS-Date/index.js
@@ -11,5 +11,9 @@ let Dates= Date.now();
 // console.log(Dates);
 
 let TheDate= new Date("09-08-2024");
-console.log(TheDate.getTime());
+// console.log(TheDate.getTime());
 
+// const Myfun = (...n) => {
+//       return `The total sum of n1,n2,n3 is ${n}`
+// }
+//  console.log(Myfun(1,2,3))
--- letsdothisdeepak/JS-For-of-Maps/maps.js
+++ letsdothisdeepak/JS-For-of-Maps/maps.js
@@ -38,5 +38,33 @@ const arr0=[]
        arr0.push(num)
         }
 })
-console.log(arr0);
+// console.log(arr0);
 
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+//   c = books.filter( (bk)=> bk.genre==='History')
+//   console.log(c);
+const d= books.filter((bk) => bk.publish>=1995 && bk.genre=='History')
+// console.log(d);
+
+const arr1=[1,2,3,4,5]
+
+const arr1_10= arr1.map((num)=> num+10);
+// console.log(arr1);
+// console.log(arr1_10);
+
+const arr1_5= arr1.forEach(function (num1) {
+    console.log(num1+5);
+    
+})
+// console.log(arr1_5);
